201eed8aa1662cb4f1a38c799a61a940472cdae6
candy <candy.yang@qibaozz.com>
2017-07-24 18:29:24 +0800

    'c'

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,7 @@ require('normalize.css/normalize.css');
 require('styles/App.css');
 
 import React from 'react';
-
+import ReactDOM from 'react-dom';
 
 // 获取图片的相关信息
 let imageDatas = require('../data/imageDatas.json');
@@ -22,18 +22,38 @@ imageDatas=(function genImageURL(imageDatasArr){
  */
 function getRangeRandom(low,high) {
 	return Math.ceil(Math.random() * (high - low) + low);
-	
-};
+}
+
+/*
+ * 获取0-30度之间的一个任意正负值
+ */
+function get30DegRandom(){
+	return((Math.random() > 0.5 ? "" : "-") + Math.ceil(Math.random() * 30));
+}
+
 
 // 单个图片组件
-class ImgFigure extends React.Component {
-	render(){
+//class ImgFigure extends React.Component {
+let ImgFigure = React.createClass({
+	render:function(){
 		
 		var styleObj = {};
 		
+		// 如果props属性中指定了这张图片的位置，则使用
+		if(this.props.arrange.pos){
+			styleObj = this.props.arrange.pos;
+		}
+		
+		// 如果图片的旋转角度有值并且不为0，天假旋转角度
+		if(this.props.arrange.rotate) {
+			(['-moz-','-webkit-','-ms-','']).forEach(function(value){
+				styleObj[value + 'transform'] = 'rotate(' + this.props.arrange.rotate + 'deg)';
+			}.bind(this));
+			
+		}
 		
 		return(
-			<figure className="img-figure">
+			<figure className="img-figure" style={styleObj}>
 				<img className="imgs" src={this.props.data.imageURL} alt={this.props.data.title} />
 				<figcation>
 					<h2 className="img-title">{this.props.data.title}</h2>
@@ -41,30 +61,33 @@ class ImgFigure extends React.Component {
 			</figure>
 		);
 	}
-};
+});
 
 // 舞台组件
-class AppComponent extends React.Component {
+//class AppComponent extends React.Component {
+let AppComponent = React.createClass({
 	Constant:{
-		centerPos:{		// 中心图片位置的取值范围
+		// 中心图片位置的取值范围
+		centerPos:{
 			left:0,
-			right:0,
+			top:0
 		},
-		hPosRange:{		// 水平方向的取值范围
+		// 水平方向的取值范围
+		hPosRange:{
 			leftSecX:[0,0],
 			rightSecX:[0,0],
 			y:[0,0]
 		},
-		vPosRange:{		// 垂直方向的取值范围
+		// 垂直方向的取值范围
+		vPosRange:{
 			x:[0,0],
 			topY:[0,0]
 		}
-		
 	},
 	
 	/*
 	 * 重新布局所有图片
-	 * @param centerIndex 指定居中排布哪个图片
+	 * @param  centerIndex指定居中排布哪个图片 
 	 */
 	rearrange:function(centerIndex){
 		var imgsArrangeArr = this.state.imgsArrangeArr,
@@ -76,7 +99,7 @@ class AppComponent extends React.Component {
 			hPosRangeRightSecX = hPosRange.rightSecX,
 			hPosRangeY = hPosRange.y,
 			vPosRangeTopY = vPosRange.topY,
-			vPosRangeX = vPosRange.X,
+			vPosRangeX = vPosRange.x,
 			
 			imgsArrangeTopArr = [],
 			topImgNum = Math.ceil(Math.random() * 2),  // 取一个或者不取
@@ -86,16 +109,22 @@ class AppComponent extends React.Component {
 		//  首先居中  centerIndex 的图片
 		imgsArrangeCenterArr[0].pos = centerPos;
 		
+		//  居中的图片不用旋转
+		imgsArrangeCenterArr[0].rotate = 0;
+		
 		//  取出要布局上侧的图片的状态信息
 		topImgSpliceIndex = Math.ceil(Math.random() * (imgsArrangeArr.length - topImgNum));
 		imgsArrangeTopArr = imgsArrangeArr.splice(topImgSpliceIndex,topImgNum );
 		
 		//  布局位于上侧的图片
 		imgsArrangeTopArr.forEach(function(value,index){
-			imgsArrangeTopArr[index].pos = {
-				top:getRangeRandom(vPosRangeTopY[0],vPosRangeTopY[1]),
-				left:getRangeRandom.(vPosRangeX[0],vPosRangeX[1])
-			}
+			imgsArrangeTopArr[index] = {
+				pos:{
+					left:getRangeRandom(vPosRangeX[0],vPosRangeX[1]),
+					top:getRangeRandom(vPosRangeTopY[0],vPosRangeTopY[1])
+				},
+				rotate:get30DegRandom()
+			};
 		});
 		
 		//  布局左右两侧的图片
@@ -109,11 +138,14 @@ class AppComponent extends React.Component {
 				hPosRangeLORX = hPosRangeRightSecX;
 			}
 			
-			imgsArrangeArr[i].pos = {
-				top:getRangeRandom(hPosRangeY[0],hPosRangeY[1]),
-				left:getRangeRandom(hPosRangeLORX[0],hPosRangeLORX[1])
-			}
-		};
+			imgsArrangeArr[i] = {
+				pos:{
+					left:getRangeRandom(hPosRangeLORX[0],hPosRangeLORX[1]),
+					top:getRangeRandom(hPosRangeY[0],hPosRangeY[1])
+				},
+				rotate:get30DegRandom()
+			};
+		}
 		
 		if(imgsArrangeTopArr && imgsArrangeTopArr[0]){
 			imgsArrangeArr.splice(topImgSpliceIndex, 0, imgsArrangeTopArr[0]);
@@ -133,7 +165,8 @@ class AppComponent extends React.Component {
 //					pos:{
 //						left:'0',
 //						top:'0'
-//					}
+//					},
+//                  rotate:0	//  旋转角度
 //				}
 			]
 		}
@@ -176,24 +209,30 @@ class AppComponent extends React.Component {
 		this.Constant.vPosRange.x[0] = haflStageW - imgW;
 		this.Constant.vPosRange.x[1] = haflStageW;
 		
-		this.rearrange()
+		this.rearrange(0);
 	},
 	
-  	render:function() {
+//	changePic:function(){
+//		var index = this.props.key;
+//		this.rearrange(index);
+//	},
+	
+	render:function(){
   		var controllerUnits = [],
   			ImgFigures = [];
 		imageDatas.forEach(function(value,index){
 			
 			if(!this.state.imgsArrangeArr[index]){
-				this.state.imgsArrangeArr[index]=[
+				this.state.imgsArrangeArr[index]={
 					pos:{
-						left:'0',
-						top:'0'
-					}
-				]
+						left:0,
+						top:0
+					},
+					rotate:0				
+				};
 			}
 			
-			ImgFigures.push(<ImgFigure data={value} ref={'imgFigure'+index} arrange={this.state.imgsArrangeArr[index]} />);
+			ImgFigures.push(<ImgFigure data={value} ref={'imgFigure'+index} arrange={this.state.imgsArrangeArr[index]} key={index} />);
 		}.bind(this));
   	
 	    return (
@@ -204,10 +243,10 @@ class AppComponent extends React.Component {
 		      	<div className="controller-nav">
 		      		controllerUnits
 		      	</div>
-		     </div>
-	    );
+		    </div>
+	    )
 	}
-}
+});
 
 AppComponent.defaultProps = {
 };
